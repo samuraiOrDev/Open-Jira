434657fb46c25d42a92a623e5b463dca245134c8
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-04-08 18:10:34 +0200

    Basic Basic Material  8-04-2023 - 18:10

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+/* Next */
 import type { AppProps } from 'next/app'
+/* Terceros */
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+/* Roboto Font */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+/* Owner */
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const basicTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    }
+  });
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
